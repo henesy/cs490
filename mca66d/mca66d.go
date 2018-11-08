acfcee22dd6e65013be49393b2a5a3ee157a0638
henesy <henesy.dev@gmail.com>
2018-11-08 14:36:45 -0600

    fix missing print

--- mca66d/mca66d.go
+++ mca66d/mca66d.go
@@ -23,13 +23,13 @@ var logfile string = "/var/log/mca66.log"
 
 /* Handle clients as they're passed from acceptor */
 func handler(conn net.Conn) {
-	// Sequentially read from the client one line at a time for commands
 	// Write errors in a cleaner manner
 	werr := func(msg string) {
 		conn.Write([]byte("Error: " + msg + "\n"))
 		time.Sleep(5 * time.Millisecond)
 	}
 
+	// Sequentially read from the client one line at a time for commands
 	for {
 		buf := make([]byte, 1024)
 		_, err := conn.Read(buf)
@@ -41,7 +41,7 @@ func handler(conn net.Conn) {
 		str := string(buf)
 		parts := strings.Fields(str)
 
-		// Each command will be two parts, [CMD args…]
+		// Should be replaced with better sanitization
 		if len(parts) > 4 {
 			//log.Print("Len parts: ", len(parts))
 			log.Print("Bad command format from client: ", str)
@@ -63,7 +63,6 @@ func handler(conn net.Conn) {
 			}
 		}
 		
-		// Need more strict parsing rules
 		switch(cmd) {
 			case "POWER":
 				// POWER 1 ON
@@ -124,6 +123,7 @@ func handler(conn net.Conn) {
 				out, err = cntl.CombinedOutput()
 				log.Print("Status info: ", out)
 			default:
+				log.Print("Error, unknown command: ", cmd)
 				conn.Write([]byte("Error, unknown command.\n"))
 				time.Sleep(5 * time.Millisecond)
 				continue
