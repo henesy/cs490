b9f36899131f71ead40b89711376b44f14b8d73a
henesy <henesy.dev@gmail.com>
2018-11-15 17:54:37 -0600

    got keepalive working! ;; need to make real ui now

--- mca66d/ws_handler.go
+++ mca66d/ws_handler.go
@@ -3,17 +3,43 @@ package main
 import (
 	"net/http"
 	"log"
+	"time"
 
 	// Extension library to stdlib that handles websockets nicely
-	""
+	"github.com/gorilla/websocket"
 )
 
 
+/* Upgrades an http to websocket connection */
 var upgrader = websocket.Upgrader{
     ReadBufferSize:  1024,
     WriteBufferSize: 1024,
 }
 
+/* Keeps a websocket connection alive */
+func keepAlive(c *websocket.Conn, timeout time.Duration) {
+    lastResponse := time.Now()
+    c.SetPongHandler(func(msg string) error {
+       lastResponse = time.Now()
+       return nil
+   })
+
+   go func() {
+     for {
+        err := c.WriteMessage(websocket.PingMessage, []byte("keepalive"))
+        if err != nil {
+            return 
+        }   
+        time.Sleep(timeout/2)
+        if(time.Now().Sub(lastResponse) > timeout) {
+            c.Close()
+            return
+        }
+    }
+  }()
+}
+
+/* Continually read messages -- supposed to keep alive? */
 func readLoop(c *websocket.Conn) {
     for {
         if _, _, err := c.NextReader(); err != nil {
@@ -32,7 +58,8 @@ func ws_handler(w http.ResponseWriter, r *http.Request) {
         return
     }
     
-	go readLoop(conn)
+    // Keep socket open for up to a week(?)
+	go keepAlive(conn, 168 * time.Hour)
     
     for {
 	    messageType, p, err := conn.ReadMessage()
